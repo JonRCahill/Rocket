5187233fbe059e7d821f0dddb6f12c7caa6f1514
= <jon@zero41.com>
2021-10-15 22:02:58 +0800

    Allow SQLx logging

--- contrib/db_pools/lib/src/pool.rs
+++ contrib/db_pools/lib/src/pool.rs
@@ -1,7 +1,10 @@
 use rocket::figment::Figment;
 
 #[allow(unused_imports)]
-use {std::time::Duration, crate::{Error, Config}};
+use {
+    crate::{Config, Error},
+    std::time::Duration,
+};
 
 /// Generic [`Database`](crate::Database) driver connection pool trait.
 ///
@@ -137,8 +140,8 @@ pub trait Pool: Sized + Send + Sync + 'static {
 
 #[cfg(feature = "deadpool")]
 mod deadpool_postgres {
-    use deadpool::managed::{Manager, Pool, PoolConfig, PoolError, Object};
-    use super::{Duration, Error, Config, Figment};
+    use super::{Config, Duration, Error, Figment};
+    use deadpool::managed::{Manager, Object, Pool, PoolConfig, PoolError};
 
     pub trait DeadManager: Manager + Sized + Send + Sync + 'static {
         fn new(config: &Config) -> Result<Self, Self::Error>;
@@ -147,7 +150,10 @@ mod deadpool_postgres {
     #[cfg(feature = "deadpool_postgres")]
     impl DeadManager for deadpool_postgres::Manager {
         fn new(config: &Config) -> Result<Self, Self::Error> {
-            Ok(Self::new(config.url.parse()?, deadpool_postgres::tokio_postgres::NoTls))
+            Ok(Self::new(
+                config.url.parse()?,
+                deadpool_postgres::tokio_postgres::NoTls,
+            ))
         }
     }
 
@@ -160,7 +166,10 @@ mod deadpool_postgres {
 
     #[rocket::async_trait]
     impl<M: DeadManager, C: From<Object<M>>> crate::Pool for Pool<M, C>
-        where M::Type: Send, C: Send + Sync + 'static, M::Error: std::error::Error
+    where
+        M::Type: Send,
+        C: Send + Sync + 'static,
+        M::Error: std::error::Error,
     {
         type Error = Error<M::Error, PoolError<M::Error>>;
 
@@ -186,9 +195,9 @@ mod deadpool_postgres {
 
 #[cfg(feature = "sqlx")]
 mod sqlx {
-    use sqlx::ConnectOptions;
-    use super::{Duration, Error, Config, Figment};
+    use super::{Config, Duration, Error, Figment};
     use rocket::config::LogLevel;
+    use sqlx::ConnectOptions;
 
     type Options<D> = <<D as sqlx::Database>::Connection as sqlx::Connection>::Options;
 
@@ -213,12 +222,9 @@ mod sqlx {
             let mut opts = config.url.parse::<Options<D>>().map_err(Error::Init)?;
             specialize(&mut opts, &config);
 
-            opts.disable_statement_logging();
             if let Ok(level) = figment.extract_inner::<LogLevel>(rocket::Config::LOG_LEVEL) {
-                if !matches!(level, LogLevel::Normal | LogLevel::Off) {
-                    opts.log_statements(level.into())
-                        .log_slow_statements(level.into(), Duration::default());
-                }
+                opts.log_statements(level.into())
+                    .log_slow_statements(level.into(), Duration::default());
             }
 
             sqlx::pool::PoolOptions::new()
@@ -239,8 +245,8 @@ mod sqlx {
 
 #[cfg(feature = "mongodb")]
 mod mongodb {
-    use mongodb::{Client, options::ClientOptions};
-    use super::{Duration, Error, Config, Figment};
+    use super::{Config, Duration, Error, Figment};
+    use mongodb::{options::ClientOptions, Client};
 
     #[rocket::async_trait]
     impl crate::Pool for Client {
@@ -250,7 +256,9 @@ mod mongodb {
 
         async fn init(figment: &Figment) -> Result<Self, Self::Error> {
             let config = figment.extract::<Config>()?;
-            let mut opts = ClientOptions::parse(&config.url).await.map_err(Error::Init)?;
+            let mut opts = ClientOptions::parse(&config.url)
+                .await
+                .map_err(Error::Init)?;
             opts.min_pool_size = config.min_connections;
             opts.max_pool_size = Some(config.max_connections as u32);
             opts.max_idle_time = config.idle_timeout.map(Duration::from_secs);
